b5392a432ac3934c4130c97460cb9a9d4e63274f
charlymech <sanchezreciocarlos99@gmail.com>
2024-11-07 21:05:30 +0100

    feat: home body set and fab routing method

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,6 +10,8 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     return Scaffold(
+      extendBody: true,
+      extendBodyBehindAppBar: true,
       body: CustomScrollView(
         slivers: [
           SliverPersistentHeader(
@@ -17,68 +19,16 @@ class Home extends StatelessWidget {
             pinned: true,
           ),
           SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Container(
-                  height: 300,
-                  color: Colors.purple,
-                ),
-                Container(
-                  height: 100,
-                  color: Colors.green,
-                ),
-                Container(
-                  height: 500,
-                  color: Colors.cyan,
-                ),
-                Container(
-                  height: 500,
-                  color: Colors.lime,
-                )
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
             ),
           ),
         ],
       ),
-      floatingActionButton: IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.add),
-        color: Color(0xFFF8F8F8),
-        iconSize: 40,
-        style: ButtonStyle(
-          backgroundColor: WidgetStateProperty.resolveWith<Color?>(
-              (Set<WidgetState> states) {
-            return Color(0xFF6A00FF);
-          }),
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.push('/add'),
+        child: const Icon(Icons.add),
       ),
     );
-
-    /* return Scaffold(
-        extendBody: true,
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-            elevation: 5,
-            scrolledUnderElevation: 5,
-            backgroundColor: Color(0xFF6A00FF),
-            toolbarHeight: 80,
-            centerTitle: true,
-            title: Text('WebViewer')),
-        body: Container(
-          color: Color(0xFF17203a), // Your desired background color
-          child: Center(child: Text('Full Screen')),
-        ),
-        floatingActionButton: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.add),
-          color: Color(0xFFF8F8F8),
-          iconSize: 40,
-          style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.resolveWith<Color?>(
-                (Set<WidgetState> states) {
-              return Color(0xFF6A00FF);
-            }),
-          ),
-        )); */
   }
 }
