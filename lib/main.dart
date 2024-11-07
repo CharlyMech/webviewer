2abeff8356ffec4822a764a43ae2f75a93fb3a75
charlymech <sanchezreciocarlos99@gmail.com>
2024-11-07 09:04:15 +0100

    feat: extend layout to top and bottom

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webviewer/app.dart';
 
 void main() {
+  // Ensure all widgets are initialized before launching the app
+  WidgetsFlutterBinding.ensureInitialized();
+  // Set the system UI overlay style at app startup
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarDividerColor: Colors.transparent,
+    ),
+  );
+  // Enable edge-to-edge
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.edgeToEdge,
+    overlays: [SystemUiOverlay.top],
+  );
+
+  // TODO -> load preferred theme (pass it as parameter to App)
   runApp(App());
 }
