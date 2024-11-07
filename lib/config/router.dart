f307f89c689f325d6747808878ccd47fdbcf91cd
charlymech <sanchezreciocarlos99@gmail.com>
2024-11-07 21:07:24 +0100

    feat: route and animation

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:webviewer/screens/add_web_screen.dart';
 import 'package:webviewer/screens/home.dart';
 import 'package:webviewer/screens/web_view_screen.dart';
 
@@ -7,6 +8,22 @@ final goRouter = GoRouter(routes: [
   GoRoute(
     path: '/',
     builder: (context, state) => const Home(),
+  ),
+  GoRoute(
+    path: '/add',
+    pageBuilder: (context, state) => CustomTransitionPage(
+        key: state.pageKey,
+        child: const AddWebScreen(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(0, 1),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child,
+          );
+        },
+        transitionDuration: const Duration(milliseconds: 300)),
   ),
   GoRoute(
     path: '/iedib',
