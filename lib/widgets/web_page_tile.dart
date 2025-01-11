05ae63d9bcf1e38fca5b08467c106495f54b5e75
CharlyMech <sanchezreciocarlos99@gmail.com>
2025-01-11 16:26:00 +0100

    feat: basic tile dcard done

--- lib/widgets/web_page_tile.dart
+++ lib/widgets/web_page_tile.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:webviewer/bloc/box/box_cubit.dart';
 import 'package:webviewer/models/web_page.dart';
 
 class WebPageTile extends StatelessWidget {
   final WebPage webPage;
   const WebPageTile({super.key, required this.webPage});
 
+  void _goUrl(BuildContext context, String uuid) {
+    context.push('/webpage?uuid=$uuid');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        webPage.title,
+    return Card(
+      child: InkWell(
+        borderRadius: BorderRadius.circular(12),
+        onTap: () => _goUrl(context, webPage.uuid),
+        child: SizedBox(
+          width: double.infinity,
+          height: 85,
+          child: Stack(alignment: AlignmentDirectional.topEnd, children: [
+            Center(
+              child: ListTile(
+                enabled: false,
+                minVerticalPadding: 10,
+                title: Text(
+                  webPage.title,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                subtitle: Text(
+                  webPage.url,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
+            IconButton(
+                onPressed: () => context.read<BoxCubit>().removeAll(),
+                icon: const Icon(Icons.delete)),
+          ]),
+        ),
       ),
-      trailing: const Icon(Icons.delete),
-      subtitle: Text(webPage.description),
-      // selected: true,
-      onTap: () {},
     );
   }
 }
