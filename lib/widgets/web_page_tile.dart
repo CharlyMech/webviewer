c7d312eaf97fc410ac0a013a4469cbacf4728ed9
CharlyMech <sanchezreciocarlos99@gmail.com>
2025-01-11 15:01:37 +0100

    feat: most simple url regex verification

--- lib/widgets/web_page_tile.dart
+++ lib/widgets/web_page_tile.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:webviewer/bloc/box/box_cubit.dart';
 import 'package:webviewer/models/web_page.dart';
 
 class WebPageTile extends StatelessWidget {
   final WebPage webPage;
   const WebPageTile({super.key, required this.webPage});
 
+  void _goUrl(BuildContext context, String uuid) {
+    context.push('/webpage?uuid=$uuid');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        webPage.title,
+    return Card(
+      color: Colors.blue,
+      child: Stack(
+        alignment: Alignment.centerRight,
+        children: [
+          ListTile(
+            title: Text(
+              webPage.title,
+              style: const TextStyle(color: Colors.white),
+            ),
+            subtitle: Text(
+              webPage.url,
+              style: const TextStyle(color: Colors.white),
+            ),
+            onTap: () => _goUrl(context, webPage.uuid),
+          ),
+          IconButton(
+              onPressed: () {
+                context.read<BoxCubit>().removeAll();
+              },
+              icon: const Icon(Icons.delete)),
+        ],
       ),
-      trailing: const Icon(Icons.delete),
-      subtitle: Text(webPage.description),
-      // selected: true,
-      onTap: () {},
     );
   }
 }
